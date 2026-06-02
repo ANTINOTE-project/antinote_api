5e0f2d6bc73909734babf4ee675e3a0535692f68
Haroun El Omri <h.elomri@proton.me>
2026-06-02 20:19:44 +0200

    Made room ID nullable

--- lib/src/models/classes/room.dart
+++ lib/src/models/classes/room.dart
@@ -1,12 +1,12 @@
 import 'dart:typed_data';
 
+import 'package:antinote/src/helpers/cache.dart';
 import 'package:antinote/src/helpers/json.dart';
 import 'package:antinote/src/helpers/visual_id.dart';
-import 'package:antinote/src/helpers/cache.dart';
 
 final class Classroom with VisualIdMixin {
   final String label;
-  final String id;
+  final String? id;
   final int count;
 
   const Classroom({required this.label, required this.id, required this.count});
